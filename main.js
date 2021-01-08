b8a1e96a2f2ed2cb5d35e8bd1d85d56a57ffee77
Alexjm66 <alexjmaart665@gmail.com>
2021-01-08 09:24:21 +0200

    Javscript

---
+++ main.js
@@ -0,0 +1,6 @@
+function onClickMenu(){
+	document.getElementById("hamburger").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("nav-bg").classList.toggle("change-bg");
+}
